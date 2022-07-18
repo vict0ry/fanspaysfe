2fdca8172314027392e5111daf1b78d539532c30
Danish <danil.mukhammad2014@gmail.com>
2022-07-18 18:24:18 +0300

    shop page

---
+++ src/pages/shop/LeftFilter.js
@@ -0,0 +1,580 @@
+import React, { useState } from 'react'
+import { Box, Button, ButtonBase, Checkbox, FormControlLabel, FormGroup, Grid, InputBase, Slider } from '@mui/material'
+import { SearchInput } from '../messages/SearchInput'
+import { Icon } from '../messages/Icon'
+import useWindowDimensions from '../../useWindowDimensions'
+import { TextField } from '@mui/material'
+import axios from 'axios'
+import { t } from 'i18next'
+
+const leftFilterStyles = {
+  cont: {
+    padding: "24px 12px",
+    borderRadius: "8px",
+    background: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    marginRight: "16px"
+  },
+  ageCont: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    marginBottom: "24px",
+    width: "100%"
+  },
+  ageContItem: {
+    marginBottom: 8,
+    display: "flex",
+    alignItems: "center"
+  },
+  title: {
+    marginBottom: "16px",
+    fontSize: 14,
+    fontWeight: 700,
+    color: "#5D5E65"
+  },
+  ageTitle: {
+    fontSize: 12,
+    fontWeight: 700,
+    marginRight: 8,
+    // maxWidth: 30
+  },
+  ageController: {
+    display: "flex",
+    alignItems: "center"
+  },
+  ageButton: {
+    minWidth: 0,
+    minHeight: 0,
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+    width: 16,
+    height: 16,
+    color: "#000",
+    background: "#F7F5F9",
+    padding: 2,
+    fontWeight: 600
+  },
+  inputAge: {
+    margin: "0 4px",
+    borderRadius: 8,
+    background: "#F7F5F9",
+    padding: "4px 6px",
+    fontSize: 12,
+    fontWeight: 600,
+    color: "#4776E6",
+    width: 28,
+    height: 24,
+  },
+
+  categories: {
+    height: 150,
+    width: "100%",
+    overflowY: "scroll",
+    '&::-webkit-scrollbar-track': {
+      background: "#ECE9F1",
+      border: "8px solid transparent",
+      backgroundClip: "content-box",
+      borderRadius: 3
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: '#4776E6',
+      borderRadius: 3,
+      border: "8px solid transparent",
+      backgroundClip: "content-box"
+    },
+    marginBottom: "24px"
+  },
+  apply_btn: {
+    color: "#4776E6",
+    fontSize: 12,
+    fontWeight: 700,
+    textTransform: "none",
+    marginRight: 8,
+    background: "#E8EFFF",
+    borderRadius: 8,
+    padding: "10px 13px"
+  },
+  clear_btn: {
+    width: 36,
+    height: 36,
+    background: "#F7F5F9",
+    borderRadius: 8,
+    minHeight: 0,
+    minWidth: 0
+  }
+}
+
+const BpIcon = () => {
+  return(
+    <span style={{
+      borderRadius: 6,
+      width: 16,
+      height: 16,
+      border: "1px solid #ECE9F1",
+    }}>
+
+    </span>
+  )
+};
+
+const BpCheckedIcon = () => {
+  return(
+    <span style={{
+      borderRadius: 6,
+      width: 16,
+      height: 16,
+      border: "1px solid #ECE9F1",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 0
+    }}>
+      <Icon name="check" />
+    </span>
+  );
+}
+
+const FormControlLabelStyles = {
+  margin: 0,
+  marginBottom: "8px",
+  '& .css-187hphe-MuiTypography-root': {
+    fontSize: 12,
+    fontWeight: 700
+  },
+  '& .css-12wnr2w-MuiButtonBase-root-MuiCheckbox-root': {
+    padding: 0,
+    marginRight: "8px",
+    borderRadius: 6
+  }
+}
+
+const LeftFilter = (props) => {
+  const {width, height} = useWindowDimensions();
+
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(10000);
+  const [price, setPrice] = useState([minPrice, maxPrice]);
+
+  return(
+    <Grid
+      item
+      sx={{
+        ...leftFilterStyles.cont,
+        display: {
+          md: "flex",
+          // xs: "none"
+        },
+        position: {
+          md: "initial",
+          xs: "fixed"
+        },
+        bottom: 0,
+        left: 0,
+        zIndex: {
+          md: "initial",
+          xs: 3
+        },
+        width: {
+          md: "224px",
+          xs: "100%"
+        },
+        padding: {
+          xs: "24px 32px",
+          md: "24px 12px"
+        },
+        borderTopLeftRadius: {
+          xs: "40px",
+          md: "8px"
+        },
+        borderTopRightRadius: {
+          xs: "40px",
+          md: "8px"
+        }
+      }}
+    >
+      <Box style={{
+        ...leftFilterStyles.ageCont,
+        ...props.isMarket ? {marginBottom: "8px"}: {}
+      }}>
+        <Box sx={{
+          ...leftFilterStyles.title,
+          display: {
+            xs: "flex",
+            md: "block"
+          },
+          width: "100%",
+          justifyContent: "space-between"
+        }}>
+          {
+            props.isMarket ? t("MARKET.PRICE") :
+            t("USERS.AGE")
+          }
+          {width < 900 &&
+          <Button
+            sx={{
+              minWidth: 0,
+              minHeight: 0
+            }}
+            onClick={() => {
+              props.setLeftMenuOpen(false);
+            }}
+          >
+            <Icon name="X" />
+          </Button>
+          }
+        </Box>
+
+
+        {!props.isMarket && <Box style={leftFilterStyles.ageContItem}>
+          <span style={leftFilterStyles.ageTitle}>{t("USERS.FROM")}</span>
+          <Box style={leftFilterStyles.ageController}>
+            <Button
+              style={leftFilterStyles.ageButton}
+              onClick={() => {
+                if(props.fromAge > 0) {
+                  props.setFromAge(props.fromAge - 1)
+                }
+              }}
+            >-</Button>
+            <InputBase
+              style={leftFilterStyles.inputAge}
+              value={props.fromAge}
+              onChange={(e) => {
+                if(Number(e.target.value) <= props.upToAge && Number(e.target.value) > 0) {
+                  props.setFromAge(Number(e.target.value));
+                }
+              }}
+            />
+            <Button
+              style={{
+                ...leftFilterStyles.ageButton,
+                ...{paddingTop: 3},
+                ...props.fromAge===props.upToAge ? {color: "#C4C4C4"}: {}
+              }}
+              onClick={() => {
+                if(props.fromAge < props.upToAge) {
+                  props.setFromAge(props.fromAge + 1)
+                }
+              }}
+            >+</Button>
+          </Box>
+        </Box>}
+
+        {!props.isMarket && <Box style={leftFilterStyles.ageContItem}>
+          <span style={leftFilterStyles.ageTitle}>{t("USERS.UP_TO")}</span>
+          <Box style={leftFilterStyles.ageController}>
+            <Button
+              style={{
+                ...leftFilterStyles.ageButton,
+                ...props.fromAge===props.upToAge ? {color: "#C4C4C4"}: {}
+              }}
+              onClick={() => {
+                if(props.fromAge < props.upToAge) {
+                  props.setUpToAge(props.upToAge-1)
+                }
+              }}
+            >-</Button>
+            <InputBase
+              style={leftFilterStyles.inputAge}
+              value={props.upToAge}
+              onChange={(e) => {
+                if(props.fromAge <= Number(e.target.value) && Number(e.target.value) <= 100) {
+                  props.setUpToAge(Number(e.target.value));
+                }
+              }}
+            />
+            <Button
+              style={{...leftFilterStyles.ageButton, ...{paddingTop: 3}}}
+              onClick={() => {
+                if(Number(props.upToAge < 99))
+                  props.setUpToAge(props.upToAge+1)
+              }}
+            >+</Button>
+          </Box>
+        </Box>}
+
+        {props.isMarket &&
+          <Box sx={{
+            display: "flex",
+            width: "100%"
+          }}>
+            <Box sx={{
+              flexGrow: 1,
+              display: "flex",
+              padding: "4px 8px",
+              background: "#F7F5F9",
+              borderRadius: "6px",
+              marginRight: "8px",
+              alignItems: "center"
+            }}>
+              <span style={{...leftFilterStyles.ageTitle, ...{fontSize: "10px", color: "#B3B3B3", margin: 0}}}>{t("USERS.FROM")}</span>
+              <InputBase
+                sx={{
+                  margin: "0 8px",
+                  fontSize: "12px",
+                  fontWeight: 600,
+                  color: "#1A051D",
+                  width: "35px",
+                  height: "16px",
+                }}
+                value={price[0]}
+                onChange={(e) => {
+                  if(price[1] >= Number(e.target.value) && Number(e.target.value) >= minPrice) {
+                    setPrice([Number(e.target.value), price[1]]);
+                  }
+                }}
+              />
+              <span style={{
+                color: "#B3B3B3",
+                fontSize: "10px",
+                fontWeight: 600,
+
+              }}>$</span>
+            </Box>
+
+            <Box sx={{
+              flexGrow: 1,
+              display: "flex",
+              padding: "4px 8px",
+              background: "#F7F5F9",
+              borderRadius: "6px",
+              alignItems: "center"
+            }}>
+              <span style={{...leftFilterStyles.ageTitle, ...{fontSize: "10px", color: "#B3B3B3", margin: 0}}}>{t("USERS.UP_TO")}</span>
+              <InputBase
+                sx={{
+                  margin: "0 8px",
+                  fontSize: "12px",
+                  fontWeight: 600,
+                  color: "#1A051D",
+                  width: "35px",
+                  height: "16px",
+                }}
+                value={price[1]}
+                onChange={(e) => {
+                  if(price[0] <= Number(e.target.value) && Number(e.target.value) <= maxPrice) {
+                    setPrice([price[0], Number(e.target.value)]);
+                  }
+                }}
+              />
+              <span style={{
+                color: "#B3B3B3",
+                fontSize: "10px",
+                fontWeight: 600,
+
+              }}>$</span>
+            </Box>
+          </Box>
+        }
+
+        {props.isMarket &&
+          <Box sx={{
+            width: "100%",
+            marginTop: "16px",
+            padding: "0 12px",
+
+          }}>
+            <Slider
+              min={minPrice}
+              max={maxPrice}
+              // getAriaLabel={() => 'Temperature range'}
+              value={price}
+              onChange={(e, value) => {
+                setPrice(value);
+              }}
+              valueLabelDisplay="auto"
+              // getAriaValueText={valuetext}
+              sx={{
+                '& .css-eg0mwd-MuiSlider-thumb': {
+                  width: "24px",
+                  height: "24px",
+                  border: "4px solid #F7F5F9",
+
+                },
+                '& .css-14pt78w-MuiSlider-rail': {
+                  backgroundColor: "#ECE9F1"
+                },
+                '& .css-eg0mwd-MuiSlider-thumb:before': {
+                  boxShadow: "none"
+                },
+                '& .css-1gv0vcd-MuiSlider-track': {
+                  backgroundColor: "#4776E6"
+                }
+              }}
+            />
+          </Box>
+        }
+
+      </Box>
+
+
+      <Box style={{width: "100%"}}>
+        <Box style={leftFilterStyles.title}>{t("USERS.CATEGORY")}</Box>
+        <Box sx={leftFilterStyles.categories}>
+          <FormGroup>
+            {props.categories.map((category) => {
+              return(
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      icon={<BpIcon />}
+                      checkedIcon={<BpCheckedIcon />}
+                      name={category}
+                      onChange={(e) => {
+                        props.setCheckedTags({
+                          ...props.checkedTags,
+                          [e.target.name]: e.target.checked
+                        });
+                      }}
+                    />
+                  }
+                  checked={props.checkedTags[category] === undefined || !props.checkedTags[category] ? false : true}
+                  label={category}
+                  sx={FormControlLabelStyles}
+                />
+              );
+            })}
+          </FormGroup>
+        </Box>
+      </Box>
+
+
+      <Box sx={{width: "100%", marginBottom: "24px"}}>
+        <Box style={leftFilterStyles.title}>{t("MARKET.TAGS")}</Box>
+        <Box sx={{
+          padding: "8px 8px",
+          borderRadius: "8px",
+          border: "1px solid #ECE9F1",
+          background: "#fff",
+          display: "flex",
+          flexWrap: "wrap",
+          maxHeight: "120px",
+          width: "100%",
+          overflowY: "auto"
+        }}>
+          <Box sx={{
+            marginRight: "8px",
+            marginBottom: "8px"
+          }}>
+            <Icon name="hashtag" />
+          </Box>
+          {Object.keys(props.checkedTags).map((tag) => {
+            if(props.checkedTags[tag]) {
+              return (
+                <Box sx={{
+                  padding: "4px 8px",
+                  background: "#E8EFFF",
+                  borderRadius: "4px",
+                  marginRight: "4px",
+                  color: "#4776E6",
+                  fontSize: "12px",
+                  fontWeight: 600,
+                  display: "flex",
+                  marginBottom: "8px"
+                }}>
+                <span style={{ marginRight: 2 }}>
+                  {tag}
+                </span>
+                  <ButtonBase onClick={() => {
+                    props.setCheckedTags({
+                      ...props.checkedTags,
+                      [tag]: false
+                    });
+                  }}>
+                    <Icon name="x" />
+                  </ButtonBase>
+                </Box>
+              );
+            }
+          })}
+
+        </Box>
+      </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <Box sx={{
+        width: {
+          md: "180px",
+          xs: "100%"
+        },
+        marginBottom: "24px"
+      }}>
+        <Box style={leftFilterStyles.title}>{t("USERS.SEARCH_BY_NICKNAME")}</Box>
+        {/*<SearchInput*/}
+        {/*  name="at"*/}
+        {/*  placeholder="Введите ник"*/}
+        {/*  state={props.findNickname}*/}
+        {/*  setState={props.setFindNickname}*/}
+        {/*/>*/}
+        {/*{console.log(props.findNickname)}*/}
+        <InputBase
+          InputProps={{disableUnderline: true}}
+          placeholder="nickname"
+          sx={{ width: '100%', padding: '0', marginLeft: 0, border: "1px solid #000" }}
+          onChange={(e) => {
+            props.setFindNickname(e.target.value)
+          }}
+          value={props.findNickname}
+        />
+      </Box>
+      <Box sx={{
+        display: "flex",
+        // width: {
+        //   md: "180px",
+        //   xs: "100%"
+        // },
+        justifyContent: {
+          md: "start",
+          xs: "center"
+        }
+      }}>
+        <Button
+          style={
+            leftFilterStyles.apply_btn}
+            onClick={() => {
+              const checked = [];
+              Object.keys(props.checkedTags).forEach(tag => {
+                if(props.checkedTags[tag]){
+                  checked.push(tag);
+                }
+              })
+              const data = {
+                fromAge: props.fromAge,
+                upToAge: props.upToAge,
+                checkedTags: checked,
+                findNickname: props.findNickname,
+                findAuthors: props.findAuthors,
+                sortBy: props.sortBy
+              };
+              console.log(data)
+              axios.post('/test/filter?age=18&nickname=test', data).then(res => console.log(res.data));
+            }}
+        >
+          {t("USERS.APPLY_FILTER")}
+        </Button>
+        <Button style={leftFilterStyles.clear_btn}>
+          <Icon name="delete" />
+        </Button>
+      </Box>
+    </Grid>
+  );
+}
+
+export { LeftFilter };
---
+++ src/pages/shop/TopFilter.js
@@ -0,0 +1,248 @@
+import { Box, ButtonBase, InputBase } from '@mui/material'
+import { SearchInput } from '../messages/SearchInput'
+import { ClickAwayListener } from '@mui/base'
+import React from 'react'
+import { Icon } from '../messages/Icon'
+import useWindowDimensions from '../../useWindowDimensions'
+import { t } from 'i18next'
+
+const styles = {
+  mainCont: {
+    marginBottom: 24,
+    display: "flex",
+    flexDirection: "column"
+  },
+  filterCont: {
+    width: "238px",
+    height: "40px",
+    background: "#fff",
+    display: "flex",
+    borderRadius: "8px",
+    border: "1px solid #ECE9F1",
+    justifyContent: "start",
+    // position: "relative"
+  },
+  filterListCont: {
+    borderRadius: 8,
+    background: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    top: "120%",
+    right: 0,
+    zIndex: 2,
+    boxShadow: "0px 2px 8px rgba(26, 5, 29, 0.1)"
+  },
+  filterIcon: {
+    minWidth: 0,
+    minHeight: 0,
+    width: "38px",
+    height: "38px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRight: "1px solid #ECE9F1",
+    borderRadius: 0,
+    background: "#fff"
+  },
+  filterIcon2: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 12,
+    transition: "200ms"
+  },
+  tag: {
+    padding: "4px 8px",
+    background: "#E8EFFF",
+    borderRadius: 4,
+    marginRight: 8,
+    color: "#4776E6",
+    fontSize: 12,
+    fontWeight: 600,
+    display: "flex",
+    marginBottom: 8
+  }
+};
+
+const TopFilter = ({sortByOpen, setSortByOpen, setSortBy, sortBy, checkedTags, setCheckedTags, setLeftMenuOpen, leftMenuOpen, findAuthors, setFindAuthors}) => {
+  const {width, height} = useWindowDimensions();
+
+  return(
+    <Box style={styles.mainCont}>
+      <Box sx={{
+        display: "flex",
+        flexDirection: {
+          xs: "column",
+          sm: "row"
+        }
+      }}>
+        <Box sx={{
+          flexGrow: 1,
+          marginRight: {
+            sm: "16px",
+            xs: 0
+          },
+          marginBottom: {
+            sm: 0,
+            xs: "8px"
+          }
+        }}>
+          {/*<SearchInput*/}
+          {/*  name="search"*/}
+          {/*  placeholder="Поиск авторов..."*/}
+          {/*  styles={{*/}
+          {/*    backgroundColor: "#fff",*/}
+          {/*    border: "1px solid #ECE9F1",*/}
+          {/*    borderRadius: 8*/}
+          {/*  }}*/}
+          {/*/>*/}
+
+          <InputBase
+            InputProps={{disableUnderline: true}}
+            placeholder="Введите ник"
+            sx={{ width: '100%', padding: '0', marginLeft: 0, border: "1px solid #000" }}
+            onChange={(e) => {
+              setFindAuthors(e.target.value)
+            }}
+            value={findAuthors}
+          />
+        </Box>
+
+        <Box
+          sx={{
+            ...styles.filterCont
+          }}
+        >
+          <ButtonBase
+            sx={{
+              ...styles.filterIcon,
+              borderRadius: {
+                xs: "50%",
+                sm: 0
+              },
+              borderRight: {
+                xs: "0",
+                sm: "1px solid #ECE9F1"
+              },
+              position: {
+                xs: "absolute",
+                sm: "relative"
+              },
+              right: {
+                xs: "16px",
+                sm: 0
+              },
+              borderTopLeftRadius: {
+                sm: "8px"
+              },
+              borderBottomLeftRadius: {
+                sm: "8px"
+              }
+            }}
+            onClick={() => {
+              setLeftMenuOpen(!leftMenuOpen)
+            }}
+          >
+            <Icon
+              name="filter"
+              color={width > 900 ? "#B3B3B3": "#000"}
+            />
+          </ButtonBase>
+
+          <ButtonBase
+            style={{display: "flex", justifyContent: "space-between", flexGrow: 1, paddingLeft: 8, position: "relative"}}
+            onClick={() => {
+              setSortByOpen(!sortByOpen)
+            }}
+          >
+            {sortByOpen &&
+            <ClickAwayListener
+              onClickAway={() => {
+                setSortByOpen(false)
+              }}
+            >
+              <Box style={styles.filterListCont}>
+                <ButtonBase
+                  style={{padding: 8, justifyContent: "start"}}
+                  onClick={() => {
+                    setSortBy(t("USERS.BY_POPULARITY") + " ⭐")
+                    setSortByOpen(!sortByOpen)
+                  }}
+                >{t("USERS.BY_POPULARITY")} ⭐</ButtonBase>
+                <ButtonBase
+                  style={{padding: 8, justifyContent: "start"}}
+                  onClick={() => {
+                    setSortBy(t("USERS.BY_NUMBER_FANS") + " 👑")
+                    setSortByOpen(!sortByOpen)
+                  }}
+                >{t("USERS.BY_NUMBER_FANS")} 👑</ButtonBase>
+                <ButtonBase
+                  style={{padding: 8, justifyContent: "start"}}
+                  onClick={() => {
+                    setSortBy(t("USERS.NEW") + " 🔥")
+                    setSortByOpen(!sortByOpen)
+                  }}
+                >{t("USERS.NEW")} 🔥️</ButtonBase>
+                <ButtonBase
+                  style={{padding: 8, justifyContent: "start"}}
+                  onClick={() => {
+                    setSortBy(t("USERS.AGE_YOUNGER"))
+                    setSortByOpen(!sortByOpen)
+                  }}
+                >{t("USERS.AGE_YOUNGER")}</ButtonBase>
+                <ButtonBase
+                  style={{padding: 8, justifyContent: "start"}}
+                  onClick={() => {
+                    setSortBy(t("USERS.AGE_OLDER"))
+                    setSortByOpen(!sortByOpen)
+                  }}
+                >{t("USERS.AGE_OLDER")}</ButtonBase>
+
+              </Box>
+            </ClickAwayListener>
+            }
+
+            <Box style={{fontSize: 12, fontWeight: 700, color: "#5D5E65"}}>
+              {sortBy}
+            </Box>
+
+            <Box
+              style={{
+                ...styles.filterIcon2,
+                ...sortByOpen ? { transform: "rotate(-180deg)" } : { transform: "rotate(0deg)" }
+              }}>
+              <Icon
+                name="triangle"
+              />
+            </Box>
+          </ButtonBase>
+        </Box>
+      </Box>
+
+      <Box style={{display: "flex", marginTop: 8, zIndex: 1, flexWrap: "wrap"}}>
+        {Object.keys(checkedTags).map((tag) => {
+          if(checkedTags[tag]) {
+            return (
+              <Box style={styles.tag}>
+                <span style={{ marginRight: 2 }}>
+                  {tag}
+                </span>
+                <ButtonBase onClick={() => {
+                  setCheckedTags({
+                    ...checkedTags,
+                    [tag]: false
+                  });
+                }}>
+                  <Icon name="x" />
+                </ButtonBase>
+              </Box>
+            );
+          }
+        })}
+      </Box>
+    </Box>
+  );
+}
+
+export {TopFilter};
